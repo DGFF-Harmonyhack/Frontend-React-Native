d0b4970fcbdd22c29c526fb785819c6272c52fd2
domitalk <dominic.m.chu@gmail.com>
2020-07-12 18:21:45 -0400

    basic nanavigation framework finished

--- App.js
+++ App.js
@@ -2,14 +2,32 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//Navigation stuff
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import ConfirmationScreen from './screens/ConfirmationScreen'
+import FollowUpScreen from './screens/FollowUpScreen'
+import HomeScreen from './screens/HomeScreen'
+import MapScreen from './screens/MapScreen'
+
+
+
+const Stack = createStackNavigator()
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+          <Stack.Screen name="FollowUp" component={FollowUpScreen} />
+          <Stack.Screen name="Map" component={MapScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
