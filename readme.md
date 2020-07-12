01a999f245d9873f7f237fce77c42d8e246e5547
domitalk <dominic.m.chu@gmail.com>
2020-07-12 18:31:18 -0400

    button example for navigation

--- readme.md
+++ readme.md
@@ -7,3 +7,11 @@ ok google how to use react native
 // look inside components and screens folders for all the stuff we need to code 
 
 // Also, let's use those issue buttons, they totally show up as activity on our profile =) 
+
+
+    {/* this is an example of how you can make buttons to navigate with a stack navi, it will auto create a back button  */}
+    {/* all you have to do is pass a string to props.navigation.navigate() that is defined in App.js in the routes  */}
+    <Button 
+        title="Go to MapScreen"
+        onPress={() => props.navigation.navigate('Map')}
+    />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,22 @@
 // Obviously the home screen with the giant button and etc 
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = props => {
+
+
+
     return (
         <View>
             <Text>The HomeScreen</Text>
+
+            {/* this is an example of how you can make buttons to navigate with a stack navi, it will auto create a back button  */}
+            {/* all you have to do is pass a string to props.navigation.navigate() that is defined in App.js in the routes  */}
+            <Button 
+                title="Go to MapScreen"
+                onPress={() => props.navigation.navigate('Map')}
+            />
         </View>
     )
 }
