2cae1d1247eaa950d6ee2876b18aadfc34b12109
domitalk <dominic.m.chu@gmail.com>
2020-07-12 22:13:23 -0400

    navigation and passing down props, big main button setup

--- components/BigMainButton.js
+++ components/BigMainButton.js
@@ -1,12 +1,24 @@
 // the please record me button #1
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-const BigMainButton = props => {
+// this is the import you need for navigation done outside of screens
+import { useNavigation } from '@react-navigation/native';
+
+
+const BigMainButton = (props) => {
+
+    // mimic the syntax and use navigation inside of a component
+    const navigation = useNavigation();
+
     return (
         <View>
             <Text>The BigMainButton</Text>
+            <Button 
+                title="Please Record Me!" 
+                onPress={() => navigation.navigate('FollowUp')}
+            />
         </View>
     )
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,70 +1,52 @@
 // Obviously the home screen with the giant button and etc 
-
 import React, { useState, useEffect } from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
+import BigMainButton from '../components/BigMainButton'
 
 const HomeScreen = props => {
-
+    const { navigation } = props
+    // js memory for saved reqs
     const [listOfSavedReqs, setListOfSavedReqs] = useState('')
+    // boolean switch to see if setListOfSavedReqs needs to be run 
     const [isThereNewSave, setIsThereNewSave] = useState(false)
 
-
+    // the componentDidMount to load up local memory into the javascript memory
     useEffect(() => {
         // check of local memory has any saved reqs
-
         // just a placeholder value for listofsavedreqs
         setListOfSavedReqs({
             1 : '1'
         })
-
         //bc of initial load, check to see if there is new value so it doesn't loop forever from the change arg in useEffect
         if(!!isThereNewSave == true) {
             setIsThereNewSave(false)
         }
+        // the watch condition to run the useEffect again is any change in the boolean isThereNewSave
     }, [isThereNewSave]);
 
     return (
         <View>
             <Text>The HomeScreen</Text>
-
             <Text>Welcome to ___________</Text>
-
-            {/* this button is going to be replaced by a custom component? */}
-            <Button 
-                title="Please Record Me!" 
-                onPress={() => props.navigation.navigate('FollowUp')}
-            />
-
-
-
+            <BigMainButton  />
             {/* i think these should probably be replaced by custom components to style + css up */}
             <Button 
                 title="See More Requests"
-                onPress={() => props.navigation.navigate('Map')}
+                onPress={() => navigation.navigate('Map')}
             />
-
-            {/* i need to pass down some props to make this different than the above map  */}
+            {/* this is how you pass down props, second arg  */}
             <Button
                 title="See Saved Requests"
-                onPress={() => props.navigation.navigate('Map')}
-                listOfSavedReqs={listOfSavedReqs}
+                onPress={() => navigation.navigate('Map', {
+                    listOfSavedReqs: listOfSavedReqs
+                })}
             />
         </View>
     )
 }
-
 const styles = StyleSheet.create({
     main: {
         flex: 1
     }
 })
-
 export default HomeScreen; 
-
-
-            // {/* this is an example of how you can make buttons to navigate with a stack navi, it will auto create a back button  */}
-            // {/* all you have to do is pass a string to props.navigation.navigate() that is defined in App.js in the routes  */}
-            // <Button 
-            //     title="Go to MapScreen"
-            //     onPress={() => props.navigation.navigate('Map')}
-            // />
