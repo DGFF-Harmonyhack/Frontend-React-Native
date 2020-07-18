5baf0c6ae8ca50e8507cc9b667c25d8c5a41dff9
GAierken <g.aierken@gmail.com>
2020-07-17 22:09:38 -0400

    form component created without styling and functioning

---
+++ components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+
+export default function Form() {
+    return (
+        <View>
+            <TextInput placeholder="Description"/>
+            <TouchableOpacity>
+                <Text>
+                    Submit
+                </Text>
+            </TouchableOpacity>          
+        </View>
+    )
+}
