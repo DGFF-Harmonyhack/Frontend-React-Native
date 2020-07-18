5097cd1d68d5ad6f9693496e4246228ecb18e925
Frank <56416023+FrankGerold@users.noreply.github.com>
2020-07-18 18:42:54 -0400

    Update components/Form.js
    seeing what happens if i commit a suggestion

--- components/Form.js
+++ components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { View, TextInput, Text, TouchableOpacity } from 'react-native';
 
 export default function Form() {
-    return (
+    return ('i am testing the suggestion button here')
         <View>
             <TextInput placeholder="Description"/>
             <TouchableOpacity>
