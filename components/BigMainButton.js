dfe2e153328a2d9b312b8a46acd102c502d8a270
GAierken <g.aierken@gmail.com>
2020-07-26 21:15:39 -0400

    added local trigger notification on bigmainbutton.js & android: usenextnotification on app.json

--- components/BigMainButton.js
+++ components/BigMainButton.js
@@ -4,17 +4,32 @@ import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 // this is the import you need for navigation done outside of screens
 import { useNavigation } from '@react-navigation/native';
-
+import * as Notifications from 'expo-notifications'
 const BigMainButton = props => {
     // mimic the syntax and use navigation inside of a component
     const navigation = useNavigation();
+
+    // notification trigger function --GA
+    const triggerNotificationHandler = () => {
+        Notifications.scheduleNotificationAsync({
+            content: {
+                title: 'first local notification',
+                body: 'hello everybody'
+            },
+            trigger:{
+                seconds: 10
+            }
+        })
+    }
     return (
         <View style={styles.button}>
             <Text style={styles.textStyle}>The BigMainButton</Text>
             <Button 
                 title="Please Record Me!" 
                 // once follow up is loaded, we can async trigger database create
-                onPress={() => navigation.navigate('FollowUp')}
+                // onPress={() => navigation.navigate('FollowUp') // bigmainbutton should trigger notification
+            //implement notification with onpress --GA
+               onPress={triggerNotificationHandler}
             />
         </View>
     )
