697c3ed004aacb9496daa09c662dd515ad8bb6f2
domitalk <dominic.m.chu@gmail.com>
2020-08-03 11:35:09 -0400

    layout elements added with state and submit handler. still need to make the api call

--- screens/FollowUpScreen.js
+++ screens/FollowUpScreen.js
@@ -1,10 +1,26 @@
 // in the diagram, this is #2, 
 // it will have all the follow up stuff for the person who pressed the button 
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useReducer } from 'react';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 
 const FollowUpScreen = props => {
+
+    const [descriptionField, setDescriptionField] = useState('')
+    const [isResolved, setIsResolved] = useState(false)
+
+
+    const submitHandler = () => {
+        // this will make an update, should probably send 
+        // user id / user uuid / event id / resolved boolean based on which button  
+
+        setDescriptionField('');
+    }
+
+    const resolutionStatusHandler = (eventStatus) => {
+        eventStatus === "Safe" ? setIsResolved(true) : setIsResolved(false)
+    }
+
     return (
         <View>
             <Text>The FollowUpScreen</Text>
@@ -15,7 +31,44 @@ const FollowUpScreen = props => {
 
             description field, manage state 
 
-            submit button, update backend, validate if one of the buttons was pressed, preview modal maybe?  */}
+            submit button, update backend, validate if one of the buttons was pressed, preview modal maybe?  
+*/}
+            <View>
+                <View>
+                    <Button 
+                        title="I AM SAFE"
+                        onPress={resolutionStatusHandler.bind(this, "Safe")}
+                        accessibilityLabel="I am safe"
+                    />
+                </View>
+                <View>
+                    <Button 
+                        title="I NEED EVIDENCE"
+                        onPress={resolutionStatusHandler.bind(this, "Help")}
+                        accessibilityLabel="I need evidence"
+                    />
+                </View>
+            </View>
+            <View>
+                <TextInput
+                    blurOnSubmit={true}
+                    {...props}
+                    editable
+                    multiline={true}
+                    numberOfLines={3}
+                    onChangeText={text => setDescriptionField(text)}
+                    value={descriptionField}
+                    placeholder="Please provide any information about what happened"
+                />
+            </View>
+            <View>
+                <Button 
+                    title="Submit" 
+                    onPress={submitHandler}
+                    accessibilityLabel="Submit your follow up."
+                />
+            </View>
+            
 
         </View>
     )
